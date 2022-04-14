3279e1ef83b5cb2a4253b8509f231b81b1b14936
Spencer Longhurst <spencerlonghurst@Spencers-MacBook-Pro.local>
2022-04-14 14:19:19 -0600

    Set up server

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json());
+server.use(cors())
+
+server.get('api/products', (req, res) => {
+  res.json({message: 'this is where my products would be displayed'})
+})
+
+server.use('/', (req, res) => {
+  res.send('Hello from my first server made from scratch')
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
